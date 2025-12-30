9588c1a921deb1f70bc1ddd8de438378d1223a35
farrelg2007 <farrelgian2007@gmail.com>
2025-12-30 18:21:12 +0700

    versi 1.1
    penambahan isi halaman pada history & crew (belum final) dan JS

---
+++ admin.js
@@ -0,0 +1,37 @@
+const logoutBtn = document.getElementById("logout_button");
+const popup = document.getElementById("logoutPopup");
+const cancel = document.getElementById("cancelLogout");
+const $confirm = document.getElementById("confirmLogout");
+
+logoutBtn.onclick = () => (popup.style.display = "block");
+cancel.onclick = () => (popup.style.display = "none");
+$confirm.onclick = () => alert("Logout");
+const menuBtn = document.getElementById("menu_button");
+const sidebar = document.getElementById("sidebar");
+const main = document.querySelector(".main");
+
+menuBtn.addEventListener("click", () => {
+    sidebar.classList.toggle("closed");
+    main.classList.toggle("full");
+});
+
+const links = document.querySelectorAll(".sidebar a");
+const pages = document.querySelectorAll(".page");
+
+links.forEach((link) => {
+    link.addEventListener("click", (e) => {
+        e.preventDefault();
+
+        const target = link.dataset.page;
+
+        // hide semua halaman
+        pages.forEach((page) => (page.style.display = "none"));
+
+        // show halaman target
+        document.getElementById(target).style.display = "block";
+
+        // aktifin menu
+        links.forEach((l) => l.classList.remove("active"));
+        link.classList.add("active");
+    });
+});
